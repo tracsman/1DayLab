e5222fe02bbad4231696353c3e05a4a0619b7ef7
Jon Ormond <tracsman@users.noreply.github.com>
2016-06-06 14:18:25 -0700

    Create AppSrvBuild.ps1

---
+++ AppSrvBuild.ps1
@@ -0,0 +1,28 @@
+# AppVM01 Server Post Build Config Script
+# PowerShell must be run as Administrator for Net Share commands to work
+
+# Turn On ICMPv4
+    New-NetFirewallRule -Name Allow_ICMPv4 -DisplayName "Allow ICMPv4" -Protocol ICMPv4 -Enabled True -Profile Any -Action Allow
+
+# Create Directory
+    New-Item "C:\WebShare" -ItemType Directory
+
+# Write out Rand.txt
+    $FileContent = "Hello, I'm the contents of a remote file on AppVM01."
+    $FileContent | Out-File -FilePath "C:\WebShare\Rand.txt" -Encoding ascii
+
+# Set Permissions on share
+    $Acl = Get-Acl "C:\WebShare"
+    $AccessRule = New-Object system.security.accesscontrol.filesystemaccessrule("Everyone","ReadAndExecute, Synchronize","ContainerInherit, ObjectInherit","InheritOnly","Allow")
+    $Acl.SetAccessRule($AccessRule)
+    Set-Acl "C:\WebShare" $Acl
+
+# Create network share
+    Net Share WebShare=C:\WebShare "/grant:Everyone,READ"
+
+# Turn Off IE Enhanced Security Configuration for Admins
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0
+
+    Write-Host
+    Write-Host "File Server Setup Successfull!" -ForegroundColor Green
+    Write-Host
