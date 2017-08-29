9a74ce981438f3b839fda015df1861e925069b1f
Jon Ormond <tracsman@users.noreply.github.com>
2017-08-29 15:32:18 -0700

    Update BootStrapRelayVM.ps1

--- UtilityVM/BootStrapRelayVM.ps1
+++ UtilityVM/BootStrapRelayVM.ps1
@@ -1,19 +1,21 @@
-
+Write-Host "1"
 $FileName = @()
 $FileName += 'putty.exe'
 $FileName += 'ChromeSetup.exe'
-
+Write-Host "2"
 Try {
-    $File = 'ChromeSetup.exe'
     $uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/'
     $LocPath = $env:USERPROFILE + "\Desktop\"
-
+Write-Host "3"
     ForEach ($File in $FileName) {
+        Write-Host $uri + $File
+        Write-Host $LocPath + $File 
         $webClient = new-object System.Net.WebClient
         $webClient.DownloadFile( $uri + $File, $LocPath + $File )
         }
     }
 Catch {
+    Write-Host "End"
     Write-Warning "Something bad happened with the download. Most likely either files are missing or you don't have internet access. Please try again, or contact your administrator."
     Return
     }
