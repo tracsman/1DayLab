a07e204c356070291d585d938b5092e2932e5c01
Jon Ormond <tracsman@users.noreply.github.com>
2017-08-29 16:03:48 -0700

    Update BootStrapRelayVM.ps1

--- UtilityVM/BootStrapRelayVM.ps1
+++ UtilityVM/BootStrapRelayVM.ps1
@@ -1,13 +1,5 @@
-Try {
-    $uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/ChromeSetup.exe'
-    $Local = "Z:\"  + "Desktop\" + "ChromeSetup.exe"
-    $webClient = new-object System.Net.WebClient
-    Write-Host "From: $uri"
-    Write-Host "To: $Local"
-    $webClient.DownloadFile( $uri, $Local )
-    }
+$uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/ChromeSetup.exe'
+$Local = "Z:\"  + "Desktop\" + "ChromeSetup.exe"
+$webClient = new-object System.Net.WebClient
+$webClient.DownloadFile( $uri, $Local )
 
-Catch {
-    Write-Warning "Something bad happened with the download. Most likely either files are missing or you don't have internet access. Please try again, or contact your administrator."
-    Return
-    }
