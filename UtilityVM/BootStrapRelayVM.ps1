bed040d27dfe46fcca7871c97b63be41fa68e76e
Jon Ormond <tracsman@users.noreply.github.com>
2017-08-29 16:04:27 -0700

    Update BootStrapRelayVM.ps1

--- UtilityVM/BootStrapRelayVM.ps1
+++ UtilityVM/BootStrapRelayVM.ps1
@@ -1,3 +1,4 @@
+# Copy, paste, run on local VM (Adjust local path as needed)
 $uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/ChromeSetup.exe'
 $Local = "Z:\"  + "Desktop\" + "ChromeSetup.exe"
 $webClient = new-object System.Net.WebClient
