8d94f9b14db3cb8d25d9a9a8779864c7a07eeb3c
Jon Ormond <tracsman@users.noreply.github.com>
2018-06-10 16:08:52 -0700

    Update user profile name

--- UtilityVM/BootStrapRelayVM.ps1
+++ UtilityVM/BootStrapRelayVM.ps1
@@ -1,6 +1,6 @@
 # Copy, paste, run on local VM (Adjust local path as needed)
 $uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/ChromeSetup.exe'
-$Local = "C:\Users\tracsman\Desktop\ChromeSetup.exe"
+$Local = "C:\Users\$env:USERNAME\Desktop\ChromeSetup.exe"
 $webClient = new-object System.Net.WebClient
 $webClient.DownloadFile( $uri, $Local )
 
