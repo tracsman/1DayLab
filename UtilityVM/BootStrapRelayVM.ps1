f8a047c65ed46605315566554326301ecd7e4bd7
Jon Ormond <jonor@microsoft.com>
2019-04-14 02:13:51 -0700

    Switch bootstrap to Chromium Edge

--- UtilityVM/BootStrapRelayVM.ps1
+++ UtilityVM/BootStrapRelayVM.ps1
@@ -1,6 +1,6 @@
 # Copy, paste, run on local VM (Adjust local path as needed)
-$uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/ChromeSetup.exe'
-$Local = "C:\Users\$env:USERNAME\Desktop\ChromeSetup.exe"
+$uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/MicrosoftEdgeSetup.exe'
+$Local = "C:\Users\$env:USERNAME\Desktop\MicrosoftEdgeSetup.exe"
 $webClient = new-object System.Net.WebClient
 $webClient.DownloadFile( $uri, $Local )
 
