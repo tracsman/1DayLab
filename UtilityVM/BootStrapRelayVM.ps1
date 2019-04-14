0a4f94f0ab62e94a5a37fffdfcadf40cd9d9c887
Jon Ormond <tracsman@users.noreply.github.com>
2019-04-14 02:15:21 -0700

    Update $HOME variable

--- UtilityVM/BootStrapRelayVM.ps1
+++ UtilityVM/BootStrapRelayVM.ps1
@@ -1,6 +1,6 @@
 # Copy, paste, run on local VM (Adjust local path as needed)
 $uri = 'https://raw.githubusercontent.com/tracsman/1DayLab/DontLook/UtilityVM/MicrosoftEdgeSetup.exe'
-$Local = "C:\Users\$env:USERNAME\Desktop\MicrosoftEdgeSetup.exe"
+$Local = "$HOME\Desktop\MicrosoftEdgeSetup.exe"
 $webClient = new-object System.Net.WebClient
 $webClient.DownloadFile( $uri, $Local )
 
