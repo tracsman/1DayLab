086dfba1ac36d81d856c4c579d13c8bf038b3cc8
Jon Ormond <tracsman@hotmail.com>
2021-03-11 19:25:19 -0800

    Update test script for cloud shell

--- UtilityVM/CSTest.ps1
+++ UtilityVM/CSTest.ps1
@@ -1,16 +1,28 @@
-"v6"
+# Start nicely
+Write-Host
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Initializing workshop environment, estimated total time < 1 minute" -ForegroundColor Cyan
+
 # Check for folder, create if not found
-$ToolPath = ".\Scripts" 
-If (-Not (Test-Path $ToolPath)){New-Item -ItemType Directory -Force -Path $ToolPath | Out-Null}
+Write-Host
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Creating Scripts Folder" -ForegroundColor Cyan
+$ScriptPath = ".\Scripts" 
+If (-Not (Test-Path $ScriptPath)){New-Item -ItemType Directory -Force -Path $ScriptPath | Out-Null}
 
-
-# Update Init.txt with Company Number
-If (-Not (Test-Path $ToolPath\Init.txt)){
-    $FileContent = "CompanyID=" + $CompanyID + "`nSubID=" + $SubID
-    Out-File -FilePath "$ToolPath\Init.txt" -Encoding ascii -InputObject $FileContent -Force
+# Create and fill Init.txt
+Write-Host
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Creating Init File" -ForegroundColor Cyan
+If (-Not (Test-Path $ScriptPath\Init.txt)){
+    $FileContent = "SubID=00000000-0000-0000-0000-000000000000" + "`nShortRegion=westus2" + "`nRGName=FWLab"
+    Out-File -FilePath "$ScriptPath\Init.txt" -Encoding ascii -InputObject $FileContent -Force
 }
 
 # Download lab files
+Write-Host
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Downloading PowerShell Scripts" -ForegroundColor Cyan
 $FileName = @()
 $FileName += 'WorkshopStep1.ps1'
 $FileName += 'WorkshopStep2.ps1'
@@ -18,8 +30,76 @@ $FileName += 'WorkshopStep3.ps1'
 $FileName += 'WorkshopStep4.ps1'
 $FileName += 'WorkshopStep5.ps1'
 $FileName += 'WorkshopStep6.ps1'
-$FileName += 'WorkshopStep7.ps1'
 $uri = 'https://raw.githubusercontent.com/tracsman/vdcWorkshop/master/Firewall/Scripts/PowerShell/'
 ForEach ($File in $FileName) {
-    Invoke-WebRequest -Uri "$uri$File" -OutFile "$ToolPath\$File"
+    Invoke-WebRequest -Uri "$uri$File" -OutFile "$ScriptPath\$File" | Out-Null
 }
+
+# Validate Environment
+Write-Host
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Validating environment:" -ForegroundColor Cyan
+
+Write-Host "  Checking Script Folder...." -NoNewline
+If (-Not (Test-Path $ScriptPath)){
+    Write-Host "Folder not found" -ForegroundColor Red
+    Return
+} Else {
+    Write-Host "Good" -ForegroundColor Green
+}
+
+Write-Host "  Checking Init File........" -NoNewline
+If (-Not (Test-Path $ScriptPath\Init.txt)){
+    Write-Host "File Not Found" -ForegroundColor Red
+    Return
+} Else {
+    Write-Host "Good" -ForegroundColor Green
+}
+
+Write-Host "  Validating File Variables:"
+If (Test-Path -Path $ScriptPath\init.txt) {
+        Get-Content $ScriptPath\init.txt | Foreach-Object{
+        $var = $_.Split('=')
+        Try {New-Variable -Name $var[0].Trim() -Value $var[1].Trim() -ErrorAction Stop}
+        Catch {Set-Variable -Name $var[0].Trim() -Value $var[1].Trim()}}}
+
+Write-Host "    Checking SubID.........." -NoNewline
+Try {$Sub = (Set-AzContext -Subscription $SubID -ErrorAction Stop).Subscription}
+Catch {
+    Write-Host "SubID not valid or unauthorized" -ForegroundColor Red
+    Return
+}
+Write-Host "Valid, Context: $($Sub.Name)" -ForegroundColor Green
+
+Write-Host "    Checking Region........." -NoNewline
+If ($null -eq (Get-AzLocation | Where-Object Location -eq $ShortRegion)) {
+    Write-Host "ShortRegion not valid or unauthorized" -ForegroundColor Red
+    Return
+} Else {
+    Write-Host "Valid" -ForegroundColor Green
+}
+
+Write-Host "    Checking RG Name........" -NoNewline
+If ($RGName.Length -le 3) {"Bad short or don't exist"}
+ElseIf ($RGName -contains " ") {"Bad Space"}
+Write-Host "Valid" -ForegroundColor Green
+
+Write-Host "  Checking Script Files....." -NoNewline
+Try {
+    Test-Path $ScriptPath\WorkshopStep1.ps1 -ErrorAction Stop
+    Test-Path $ScriptPath\WorkshopStep2.ps1 -ErrorAction Stop
+    Test-Path $ScriptPath\WorkshopStep3.ps1 -ErrorAction Stop
+    Test-Path $ScriptPath\WorkshopStep4.ps1 -ErrorAction Stop
+    Test-Path $ScriptPath\WorkshopStep5.ps1 -ErrorAction Stop
+    Test-Path $ScriptPath\WorkshopStep6.ps1 -ErrorAction Stop
+}
+Catch {
+    Write-Host "One or more script files Not Found" -ForegroundColor Red
+    Return
+}
+Write-Host "All present" -ForegroundColor Green
+
+# End nicely
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Environment initialization completed successfully" -ForegroundColor Green
+Write-Host
